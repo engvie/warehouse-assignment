ccc863aaf65ef0d2e2d4708150dc83721d8d0df1
Albert Engvie <albert.engvie@gmail.com>
2022-08-30 00:48:09 +0200

    Add routes

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ImportController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +19,23 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('products', [ProductController::class, 'index']);
+    Route::post('order/{product}', [OrderController::class, 'order']);
+    
+    Route::resource('product-parts', ProductInventory::class);
+    
+    Route::post('/import/products', [ImportController::class, 'importProducts']);
+    Route::post('/import/inventory', [ImportController::class, 'importInventories']);
 });
 
-Route::resource('products', ProductController::class);
+
