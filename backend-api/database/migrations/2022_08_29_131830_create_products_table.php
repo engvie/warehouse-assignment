809dc9831fe2cc98fd862d911a6df4ea09913c13
Albert Engvie <albert.engvie@gmail.com>
2022-08-30 00:48:26 +0200

    Add migrations and seeds

---
+++ backend-api/database/migrations/2022_08_29_131830_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->default('https://placehold.jp/600x600.png')->nullable();
+            $table->integer('price')->default(null)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ backend-api/database/migrations/2022_08_29_132016_create_product_components_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_components', function (Blueprint $table) {
+            $table->integer('id')->unsigned();
+            $table->integer('product_id')->unsigned()->nullable();
+            $table->integer('amount_of')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_components');
+    }
+};
